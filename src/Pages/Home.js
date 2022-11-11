862fe6bfa11d939c63c8f432c8dde2308e187054
rakibul181 <rakibulhasan0641@gmail.com>
2022-11-11 06:33:09 +0600

    Home page hero added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        
+        <div>
+            <div className="hero min-h-screen bg-base-200 bg-[url('https://images.unsplash.com/photo-1603298792146-0b8aa2ce95ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80')]">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Hello there</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
