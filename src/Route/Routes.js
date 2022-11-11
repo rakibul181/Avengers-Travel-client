c02db19b81609d86a4df960e97df598fe2d16ddb
rakibul181 <rakibulhasan0641@gmail.com>
2022-11-11 07:35:27 +0600

    services data loded and also limit services

--- src/Route/Routes.js
+++ src/Route/Routes.js
@@ -1,4 +1,5 @@
 import AddService from "../Component/AddService";
+import Services from "../Component/Services";
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../Layout/Main");
@@ -11,11 +12,19 @@ export const router = createBrowserRouter([
         children:[
             {
                 path:'/',
-                element:<Home></Home>
+                element:<Home></Home>,
+                loader:()=>fetch('http://localhost:5000/3service')
+
             },
             {
                 path:'add-service',
                 element:<AddService></AddService>
+
+            },
+            {
+                path:'services',
+                element:<Services></Services>,
+                loader: ()=> fetch('http://localhost:5000/services')
 
             }
 
